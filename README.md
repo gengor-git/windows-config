d53e2a9580652ea907ac74ef87c81f2c283d517a
Martin Palmowski <martin.palmowski@gmail.com>
2020-09-19 19:14:53 +0200

    Added MiKTeX installer and settings.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# windows-config
+# Windows Config Stuff
 
 Scripts and configs for my Windows environment
 
-## Features
+## setup_documentation.ps1
+
+Script to install my documentation tools and make some relevant config
+settings.
 
 - Create Download directory (if not present)
-- Download Pandoc (portable zip)
-- Download MiKTeX (installer)
-- Unzip Pandoc
-- Add Pandoc to path variable
+- Download Pandoc & install as portable
+- Download MiKTeX (installer) and silent install as portable
--- setup-documentation.ps1
+++ setup-documentation.ps1
@@ -2,15 +2,21 @@
 Installer to setup MikTeX-Environment, Pandoc and matching environment variables.
 Currently Work-in-Progress.
 #>
+
+$do_pandoc = $true
+$do_miktex = $true
+
 $download_folder = "$env:USERPROFILE\Downloads\documentation-downloads"
 
 $target_root_folder = "C:\Portable"
 $target_miktex_folder = "$target_root_folder\miktex"
-$target_pandoc_folder ="$target_root_folder\pandoc"
+$target_pandoc_folder = "$target_root_folder\pandoc"
 
 $miktex_installer = "$download_folder\miktex-portable.exe"
 $miktex_download_uri = "https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.6.29-x64.exe"
 $miktext_install_params = "--portable=`"$target_miktex_folder`" --auto-install=yes --unattended"
+$target_miktex_path = "C:\Portable\miktex\texmfs\install\miktex\bin\x64"
+$target_pandoc_path = ""
 
 $pandoc_installer = "$download_folder\pandoc-2.10.1-windows-x86_64.zip"
 $pandoc_download_uri = "https://github.com/jgm/pandoc/releases/download/2.10.1/pandoc-2.10.1-windows-x86_64.zip"
@@ -18,9 +24,10 @@ $pandoc_download_uri = "https://github.com/jgm/pandoc/releases/download/2.10.1/p
 $user_path = [System.Environment]::GetEnvironmentVariable("Path", "User")
 
 # Download directory needs to be present to store the installers.
-if(Test-Path -Path $download_folder) {
+if (Test-Path -Path $download_folder) {
     Write-Host "Download directory exists."
-} else {
+}
+else {
     Write-Host "Download directory does not exist. Creating new one at $download_folder."
     New-Item -Path $download_folder -ItemType Directory 
 }
@@ -28,32 +35,50 @@ if(Test-Path -Path $download_folder) {
 # Download web client, because Invoke-WebRequest is too slow.
 $dl = New-Object System.Net.WebClient
 
-# Pandoc download
-Write-Host "Downloading Pandoc portable zip ... " -NoNewline
-#$dl.DownloadFile($pandoc_download_uri, $pandoc_installer)
-Write-Host "done."
-Write-Host "Unzipping Pandoc ..." -NoNewline
-Expand-Archive -Path $pandoc_installer -DestinationPath $target_pandoc_folder -Force
-Write-Host "done."
-if ($user_path.Contains($target_pandoc_path)) {
-    Write-Host "Pandoc already in Path. Check if this is correct!"
-} else {
-    Write-Host "Will add Pandoc to PATH variable and save later."
+if ($do_pandoc) {
+    Write-Host "====PANDOC===="
+    if (-not (Test-Path -Path $pandoc_installer)) {
+        Write-Host "Downloading Pandoc portable zip ... " -NoNewline
+        $dl.DownloadFile($pandoc_download_uri, $pandoc_installer)
+        Write-Host "done."
+    }
+    Write-Host "Unzipping Pandoc ..." -NoNewline
+    Expand-Archive -Path $pandoc_installer -DestinationPath $target_pandoc_folder -Force
+    Write-Host "done."
     $pandoc_subfolder = Get-ChildItem -Path $target_pandoc_folder
     $target_pandoc_path = $target_pandoc_folder + "\" + $pandoc_subfolder[0].name 
-    $user_path += ";"+$target_pandoc_path
+    if ($user_path.Contains($target_pandoc_path)) {
+        Write-Warning "Pandoc is already in the PATH. Check if this is correct!"
+    }
+    else {
+        Write-Host "Will add Pandoc to PATH variable and save later."
+        $user_path += ";" + $target_pandoc_path
+    }
 }
+
+if ($do_miktex) {
+    Write-Host "====MIKTEX===="
+    if (-not (Test-Path -Path $miktex_installer)) {
+        Write-Host "Downloading MikTeX installer ... " -NoNewline
+        $dl.DownloadFile($miktex_download_uri, $miktex_installer)
+        Write-Host "done."
+    }
+    Write-Host "Installing MiKTeX as portable ... " -NoNewline
+    #Start-Process -FilePath $miktex_installer -ArgumentList $miktext_install_params -NoNewWindow -Wait
+    Write-Host "done."
+
+    if ($user_path.Contains($target_miktex_path)) {
+        Write-Warning "MiKTeX already in PATH. Check if this is correct!"
+    }
+    else {
+        Write-Host "Will add MiKTeX bin to PATH variable and save later."
+        $user_path += ";" + $target_miktex_path
+    }
+}
+
+
 
 # Save all the changes to the path environment for the user.
 Write-Host "Save path environment ... " -NoNewline
 [System.Environment]::SetEnvironmentVariable("Path", $user_path, "User")
 Write-Host "done."
-
-# MiKTeX download
-Write-Host "Downloading MikTeX installer ... " -NoNewline
-#$dl.DownloadFile($miktex_download_uri, $miktex_installer)
-Write-Host "done."
-
-
-
-
